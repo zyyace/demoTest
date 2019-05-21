57a3393f58f7ddcc622eb5fecf2c0dfe85533e79
Ace <ace.zhou@impressivecn.com>
2019-05-21 20:38:43 +0800

    project init

---
+++ android/app/src/main/java/com/demotest/MainApplication.java
@@ -0,0 +1,63 @@
+package com.demotest;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+import com.RNFetchBlob.RNFetchBlobPackage;
+import fr.greweb.reactnativeviewshot.RNViewShotPackage;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
+import com.beefe.picker.PickerViewPackage;
+import me.listenzz.modal.TranslucentModalReactPackage;
+import com.reactcommunity.rnlanguages.RNLanguagesPackage;
+import com.reactnative.ivpusic.imagepicker.PickerPackage;
+import com.rnfs.RNFSPackage;
+import com.dylanvann.fastimage.FastImageViewPackage;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+            new RNFetchBlobPackage(),
+            new RNViewShotPackage(),
+            new SplashScreenReactPackage(),
+            new PickerViewPackage(),
+            new TranslucentModalReactPackage(),
+            new RNLanguagesPackage(),
+            new PickerPackage(),
+            new RNFSPackage(),
+            new FastImageViewPackage()
+      );
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
+}
